108acd0ae9496a811f9b4041dd5a3e394390f367
Austin Lasseter <31504432+austinlasseter@users.noreply.github.com>
2019-02-11 10:54:56 -0500

    I changed the title of my graphic
    put your novel here if you want to

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Set up the chart
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'your item here']
 
 bitterness = go.Bar(
     x=beers,
-    y=[35, 60, 85, 85],
+    y=[35, 60, 85, 75],
     name='IBU',
-    marker={'color':'darkblue'}
+    marker={'color':'lightgreen'}
 )
 alcohol = go.Bar(
     x=beers,
-    y=[5.4, 7.1, 9.2, 11.5],
+    y=[5.4, 7.1, 9.2, 4.3],
     name='ABV',
-    marker={'color':'blue'}
+    marker={'color':'pink'}
 )
 
 beer_data = [bitterness, alcohol]
@@ -33,7 +33,7 @@ app = dash.Dash()
 server = app.server
 
 app.layout = html.Div(children=[
-    html.H1('Flying Dog Brewery'),
+    html.H1('something completely different'),
     dcc.Graph(
         id='flyingdog',
         figure=beer_fig
