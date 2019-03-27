634c82b1061bd3955ff252c5afdd19f7462f5e1b
Austin Lasseter <31504432+austinlasseter@users.noreply.github.com>
2019-03-27 08:15:48 -0400

    reset the app after last night's class

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Set up the chart
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Some other beer']
+beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
 
 bitterness = go.Bar(
     x=beers,
-    y=[35, 60, 85, 65],
+    y=[35, 60, 85, 75],
     name='IBU',
-    marker={'color':'lightblue'}
+    marker={'color':'red'}
 )
 alcohol = go.Bar(
     x=beers,
-    y=[5.4, 7.1, 9.2, 11.3],
+    y=[5.4, 7.1, 9.2, 4.3],
     name='ABV',
-    marker={'color':'darkgreen'}
+    marker={'color':'blue'}
 )
 
 beer_data = [bitterness, alcohol]
