ce5a14cfc74e7216dffebcde213dad7c44ac4c47
Austin Lasseter <austinlasseter@gmail.com>
2019-08-28 16:22:52 -0400

    updated requirements.txt

--- app.py
+++ app.py
@@ -3,48 +3,59 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 
-########### Set up the chart
+########### Define your variables
 beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
 ibu_values=[35, 60, 85, 75]
 abv_values=[5.4, 7.1, 9.2, 4.3]
 color1='lightblue'
 color2='darkgreen'
+mytitle='Beer Comparison'
+tabtitle='beer!'
+myheading='Flying Dog Beers'
+label1='IBU'
+label2='ABV'
+githublink='https://github.com/austinlasseter/flying-dog-beers'
+sourceurl='https://www.flyingdog.com/beers/'
 
+########### Set up the chart
 bitterness = go.Bar(
     x=beers,
     y=ibu_values,
-    name='IBU',
+    name=label1,
     marker={'color':color1}
 )
 alcohol = go.Bar(
     x=beers,
     y=abv_values,
-    name='ABV',
+    name=label2,
     marker={'color':color2}
 )
 
 beer_data = [bitterness, alcohol]
 beer_layout = go.Layout(
     barmode='group',
-    title = 'Beer Comparison'
+    title = mytitle
 )
 
 beer_fig = go.Figure(data=beer_data, layout=beer_layout)
 
-########### Display the chart
 
-app = dash.Dash()
+########### Initiate the app
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
+app.title=tabtitle
 
+########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1('Flying Dog Beers'),
+    html.H1(myheading),
     dcc.Graph(
         id='flyingdog',
         figure=beer_fig
     ),
-    html.A('Code on Github', href='https://github.com/austinlasseter/flying-dog-beers'),
+    html.A('Code on Github', href=githublink),
     html.Br(),
-    html.A('Data Source', href='https://www.flyingdog.com/beers/'),
+    html.A('Data Source', href=sourceurl),
     ]
 )
 
