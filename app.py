5802240dedf8b3739ec462fe7ad27693e5425d4b
Austin Lasseter <31504432+austinlasseter@users.noreply.github.com>
2020-10-01 22:47:30 -0400

    changed the color on the bar chart

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import plotly.graph_objs as go
 beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
 ibu_values=[35, 60, 85, 75]
 abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
+color1='red'
+color2='yellow'
 mytitle='Beer Comparison'
 tabtitle='beer!'
 myheading='Flying Dog Beers'
